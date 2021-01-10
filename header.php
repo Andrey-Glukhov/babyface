afbeab795cc574af6b5df7c71ba28edfe90eb97f
Eliza <gluhova.liza@gmail.com>
2021-01-10 16:21:51 +0100

    placeholder

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1" >
+  <meta name="keywords" content="babyface" />
+  <meta name="description" content="babyface" />
+
+  <title>babyface</title>
+
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+  <?php wp_head(); ?>
+</head>
+
+<?php
+if(is_front_page()):
+  $baby_classes = array('babyface_front_class', 'front_class');
+else:
+  $baby_classes = array('no_babyface_front_class');
+endif;
+?>
+
+<body <?php body_class($baby_classes); ?>>
+  <header>
+	</header>
---
+++ placeholder.php
@@ -0,0 +1,29 @@
+<?php
+/**
+*Template Name: Placeholder Page
+*/
+get_header();
+
+?>
+
+<div class="container-fluid placeholder_background">
+  <div class="row justify-content-center">
+    <div class="col-md-3 baby_logo">
+      <img src="http://localhost:8888/babyface/wordpress/wp-content/themes/babyface/img/BF_INSTA09.png">
+    </div>
+  </div>
+  <div class="row justify-content-center">
+    <div class="col-md-4 baby_intro">
+      <h1>
+        On 21 January, babyface collective will launch our hybrid exhibition:
+        Don't Hold Your Breath!</h1>
+        <p>A marvellously diverse and international union of 42 photographers from
+          the Royal Academy of Art The Hague will present their stories on babyface.space
+          and via a live broadcast, on piptv.nl. Stay tuned!
+        </p>
+      </div>
+
+    </div>
+  </div>
+
+  <?php get_footer(); ?>
